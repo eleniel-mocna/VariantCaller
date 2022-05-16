4dcc548882eb28fa286ca38553a575ce06322753
eleniel-mocna <soukup.sam@gmail.com>
2022-05-16 16:07:35 +0200

    Some docing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,14 @@
+/**
+ * This is a module for a Variant Caller with a special for paired read variant calling.
+ *
+ * Now this is only an application not prepared for expansion. It's main structure is as follows:
+ * - ReadsProvider object provides Read[] objects via the getNext() method
+ * - Core gets the Read objects from ReadProvider and analyzes them compared to the Reference object
+ * - Core sends found variants to the VariantsManager
+ * - VariantsManager gathers the Variants and produces them via the output() method.
+ * - Reports writer gets an Iterable of Variants from the VariantsManager and outputs the output string via the
+ *      writeVarints() method.
+ */
 module cz.cuni.mff.soukups3.VariantCaller {
 
 }
